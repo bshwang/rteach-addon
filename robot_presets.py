23b3b40a75badc227d07bc0fe9c8975329cdf9bb
bshwang <hbs8409@gmail.com>
2025-06-17 11:01:35 +0900

    Update robot_presets.py

--- robot_presets.py
+++ robot_presets.py
@@ -1,6 +1,27 @@
 import math
 
 ROBOT_CONFIGS = {
+    "ur16e": {
+        "armature": "UR16e_Arm",
+        "axes": ["z", "x", "x", "z", "x", "z"],
+        "joint_limits_deg": [[-360, 360]] * 6,
+        "stage_joints": []
+    },
+    "ur5e": {
+        "armature": "UR5e_Arm",
+        "axes": ["z", "x", "x", "z", "x", "z"],
+        "joint_limits_deg": [[-360, 360]] * 6,
+        "stage_joints": []
+    },
+    "iiwa14": {
+        "armature": "KUKA_iiwa14_Arm",
+        "axes": ["y"] * 7,
+        "joint_limits_deg": [
+            (-170, 170), (-120, 120), (-170, 170), (-120, 120),
+            (-170, 170), (-120, 120), (-175, 175)
+        ],
+        "stage_joints": []
+    },
     "robot_prb": {
         "armature": "KUKA_iiwa14_Arm",
         "axes": ["y"] * 7,
@@ -13,20 +34,20 @@ ROBOT_CONFIGS = {
             ("joint_ev_y", "EV_Y", "LENGTH", -0.4, 0.0),
             ("joint_stage_x", "Stage_X", "LENGTH", 0.0, 0.4),
             ("joint_stage_y", "Stage_Y", "LENGTH", -0.12, 0.28),
+            ("joint_stage_z", "Stage_Z", "LENGTH", -0.25, 0.55),
             ("joint_holder_tilt", "Holder_Tilt", "ROTATION", 0.0, math.radians(35)),
             ("joint_holder_rot", "Holder_Rot", "ROTATION", 0.0, math.radians(135)),
         ]
     },
     "robot_pmbot": {
-        "armature": "UR16e_Arm",
+        "armature": "UR16e_Arm",  # 주 로봇 기준 (UR5e도 활용 가능)
         "axes": ["z", "x", "x", "z", "x", "z"],
-        "joint_limits_deg": [
-            (-360, 360)] * 6,
+        "joint_limits_deg": [[-360, 360]] * 6,
         "stage_joints": [
-            ("joint_z", "Z Axis", "LENGTH", 0.0, 1.0),
-            ("joint_x", "X Axis", "LENGTH", -0.5, 0.5),
+            ("joint_z", "Elevation", "LENGTH", 0.0, 1.0),
             ("joint_rot", "Rotation", "ROTATION", -math.pi, math.pi),
-            ("joint_torso", "Torso", "ROTATION", -math.pi/2, math.pi/2)
+            ("joint_x", "Linear", "LENGTH", -0.5, 0.5),
+            ("joint_torso", "Outtrigger", "ROTATION", -math.pi/2, math.pi/2)
         ]
     }
 }
